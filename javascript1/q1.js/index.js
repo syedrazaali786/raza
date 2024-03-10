7dd14099daa7cab1f1572469bc87fd56ba7c0efd
syedrazaali786 <161198675+syedrazaali786@users.noreply.github.com>
2024-03-10 20:57:13 +0500

    Add files via upload

---
+++ javascript1/q1.js/index.js
@@ -0,0 +1,7 @@
+var num = prompt("please enter a number","");
+for(var i=1;i<=19;i ++) {
+    document.write("<div>");
+
+   document.write(i + "x" + num + "=" + i*num);
+   document.write("</div>");
+}

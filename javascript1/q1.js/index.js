438de905feb0dac73acefdaa697c041e55237e80
syedrazaali786 <161198675+syedrazaali786@users.noreply.github.com>
2024-03-18 05:19:55 +0500

    Delete javascript1/q1.js directory

--- javascript1/q1.js/index.js
+++
@@ -1,7 +0,0 @@
-var num = prompt("please enter a number","");
-for(var i=1;i<=19;i ++) {
-    document.write("<div>");
-
-   document.write(i + "x" + num + "=" + i*num);
-   document.write("</div>");
-}
